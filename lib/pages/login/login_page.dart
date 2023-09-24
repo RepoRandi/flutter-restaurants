5c00b6149a6867853dd930b12581659d4b529912
Randi <randimaulanaakbar@gmail.com>
2023-09-24 19:45:43 +0700

    feat: refactor login page

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -20,7 +20,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login to Find Restaurants")),
+      appBar: AppBar(
+          centerTitle: true, title: const Text("Login to Find Restaurants")),
       body: SingleChildScrollView(
         controller: ScrollController(),
         child: Container(
