8799137d12286b42b9aee22cf15e0bde5f37cd96
Randi <randimaulanaakbar@gmail.com>
2023-09-25 17:25:43 +0700

    feat: change sdk & api key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("")
+    GMSServices.provideAPIKey("AIzaSyC_tzM8Wrn8LedtLRMArwKeZ0UbOV5s8AY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
